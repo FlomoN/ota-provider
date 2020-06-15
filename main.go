ab5483934f7755228213e0295b0e3c017f514715
FlomoN <flo.giger@gmail.com>
2020-06-15 18:44:29 +0200

    add react app and read json file

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
 )
 
+type Watcher struct {
+	Repo   string
+	Device string
+	Id     int
+}
+
+type Store struct {
+	Init    bool
+	GhName  string
+	GhToken string
+	Watch   []Watcher
+}
+
+var store Store
+
 func main() {
+
+	loadData()
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
@@ -12,4 +36,24 @@ func main() {
 	})
 
 	r.Run(":3000")
+}
+
+func loadData() {
+
+	pwd, _ := os.Getwd()
+	fmt.Println(pwd)
+
+	f, err := ioutil.ReadFile(filepath.Join(pwd, "./data/store.json"))
+	if os.IsNotExist(err) {
+		os.Mkdir(filepath.Join(pwd, "./data"), 0777)
+		_f, _ := os.Create(filepath.Join(pwd, "./data/store.json"))
+		store = Store{false, "", "", []Watcher{}}
+		x, _ := json.Marshal(store)
+		_f.Write(x)
+		_f.Close()
+	} else {
+		fmt.Println(string(f))
+		err = json.Unmarshal(f, &store)
+	}
+
 }
